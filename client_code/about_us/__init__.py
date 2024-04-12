04b8f02b796ba5a97ce95030faabbdb795736b32
govindapatro2000@gmail.com <govindapatro2000@gmail.com>
2024-04-12 06:09:19 +0000

    Edited settings and about_us

--- client_code/about_us/__init__.py
+++ client_code/about_us/__init__.py
@@ -6,5 +6,27 @@ class about_us(about_usTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.card_1_copy.visible = False
+    self.card_3_copy.visible = False
+    self.card_5_copy.visible = False
 
-    # Any code you write here will run before the form opens.
+  def link_8_click(self, **event_args):
+    self.card_1_copy.visible = not self.card_1_copy.visible
+    self.card_3_copy.visible = False
+    self.card_5_copy.visible = False
+
+  def link_13_click(self, **event_args):
+    self.card_3_copy.visible = not self.card_3_copy.visible
+    self.card_1_copy.visible = False
+    self.card_5_copy.visible = False
+
+  def link_15_click(self, **event_args):
+    self.card_5_copy.visible = not self.card_5_copy.visible
+    self.card_1_copy.visible = False
+    self.card_3_copy.visible = False
+
+  def link_16_click(self, **event_args):
+    pass
+
+  def button_3_click(self, **event_args):
+    open_form('Login')
