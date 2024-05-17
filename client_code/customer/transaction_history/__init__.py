3ff6a7914cc500a8fc8366eeea29cefca5fb3e61
kohilain16@gmail.com <kohilain16@gmail.com>
2024-05-17 09:36:23 +0000

    Edited customer.Viewprofile, customer.auto_topup, customer.deposit, customer.transaction_history and 2 more

--- client_code/customer/transaction_history/__init__.py
+++ client_code/customer/transaction_history/__init__.py
@@ -137,7 +137,7 @@ class transaction_history(transaction_historyTemplate):
 
     def link_1_click(self, **event_args):
         """This method is called when the link is clicked"""
-        open_form('customer',user=self.user)
+        open_form('customer_page',user=self.user)
 
     def link_24_click(self, **event_args):
         """This method is called when the link is clicked"""
